21ad2fa8a7975c3276dcb9b02a39a59a45e0013f
beaaustria <bea4freelance@gmail.com>
2025-01-27 18:03:01 +0800

    Allow navigation to signup screen

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,12 +1,15 @@
-import { ScrollView, Text, View, KeyboardAvoidingView } from "react-native";
-import { Formik, Form, Field } from "formik";
+import { Text, View, KeyboardAvoidingView } from "react-native";
+import { Formik } from "formik";
 import * as Yup from 'yup';
 import TextInput from "../../components/TextInput";
 import Button from "../../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import { ScreenRef } from "../../helper/ScreenRef";
 
 const Login = () => {
+    const navigation = useNavigation();
 
-    let validationSchema = Yup.object().shape({
+    const validationSchema = Yup.object().shape({
         logInEmail: Yup.string().email('Invalid email').required('Required'),
         logInPassword: Yup.string()
             .required('Required')
@@ -16,7 +19,7 @@ const Login = () => {
         <KeyboardAvoidingView
             className="flex-1 items-center justify-center w-full px-10"
         >
-            <Text className="text-blue-600 font-poppins font-semibold text-[32px]">Welcome</Text>
+            <Text className="text-blue-600 font-poppins font-semibold text-[32px] mb-2">Welcome</Text>
             <View className="flex justify-center items-center w-full">
                 <Formik
                     initialValues={{
@@ -25,18 +28,26 @@ const Login = () => {
                     }}
                     validationSchema={validationSchema}
                     onSubmit={(values: any, { resetForm }: any) => {
+                        console.log(values)
                         resetForm()
                     }}
                 >
                     {({ handleSubmit }) => (
-                        <View className="w-full">
+                        <View className="w-full flex flex-col gap-2">
                             <TextInput
-                                name={"logInEmail"}
+                                name="logInEmail"
+                                placeHolder="Email"
+                                label="Email"
                             />
-                            <TextInput
-                                name={"logInPassword"}
-                                isSecured={true}
-                            />
+                            <View>
+                                <TextInput
+                                    name={"logInPassword"}
+                                    isSecured={true}
+                                    label="Password"
+                                    placeHolder="Password"
+                                />
+                                <Text className="text-blue-600">Forgot Password</Text>
+                            </View>
                             <View className="mt-4">
                                 <Button
                                     onPress={handleSubmit}
@@ -47,6 +58,14 @@ const Login = () => {
                     )}
                 </Formik>
             </View>
+            <Text className="mt-2">Don't have an account?
+                <Text
+                    className="text-blue-600"
+                    onPress={() => navigation.navigate(ScreenRef.Signup)}
+                >
+                    Register here
+                </Text>
+            </Text>
         </KeyboardAvoidingView >
     )
 }
